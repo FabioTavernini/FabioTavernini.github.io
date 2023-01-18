610424cbefbb360d27c9c07241c2f24a789f1801
Es-Tessinerbrot <122873863+Es-Tessinerbrot@users.noreply.github.com>
2023-01-18 22:15:19 +0100

    updated social page

--- Webpage/inc/navbar/navbar.js
+++ Webpage/inc/navbar/navbar.js
@@ -6,7 +6,7 @@ document.write('\
     width: 100%;\
     color: white;\
     text-decoration: none;\
-    font-size: 30px;\
+    font-size: 15px;\
     font-family: monospace;\
 }\
 .navbar > a:hover{\
@@ -24,5 +24,6 @@ document.write('\
     <a href="./index.html">HOME</a> \
     <a href="./Projects.html">PROJEKTE</a> \
     <a href="./About.html">ABOUT</a> \
+    <a href="./social.html">Social</a> \
 </nav>\
 ');
