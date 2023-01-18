04d8f44feae0e549597bd590e29bc94a22d29663
Es-Tessinerbrot <122873863+Es-Tessinerbrot@users.noreply.github.com>
2023-01-18 15:35:57 +0100

    added home

---
+++ JS/scriptwelcome.js
@@ -0,0 +1,12 @@
+// Add event listener on keydown
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+
+    if (name != '') {
+
+        location.href = "./webpage/index.html";
+
+    }
+
+});
