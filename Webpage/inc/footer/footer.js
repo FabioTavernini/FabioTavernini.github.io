36c305b8037e385f21223c9edd3714206a7f73d8
Es-Tessinerbrot <122873863+Es-Tessinerbrot@users.noreply.github.com>
2023-01-18 16:50:19 +0100

    exported navbar and footer

---
+++ Webpage/inc/footer/footer.js
@@ -0,0 +1,25 @@
+document.write('\
+<style>\
+#footerlink {\
+    text-decoration: none;\
+    color: black;\
+}\
+\
+#footer {\
+    clear: both;\
+    text-align: center;\
+    width: 100%;\
+    position: absolute;\
+    font-family: monospace;\
+    color: black;\
+    bottom: 5px;\
+    left: 0px;\
+}\
+</style>\
+\
+<footer id="footer">\
+<a>Fabio Tavernini - 2023</a>\
+<br>\
+<a id="footerlink" href="https://github.com/Es-Tessinerbrot/EsTessinerbrot.github.io">GitHub</a>\
+</footer>\
+');
---
+++ Webpage/inc/navbar/navbar.js
@@ -0,0 +1,26 @@
+document.write('\
+\
+<style>\
+\
+.navbar > a {\
+    width: 100%;\
+    color: black;\
+    text-decoration: none;\
+    font-size: 30px;\
+    font-family: monospace;\
+}\
+.navbar{\
+    text-align:center;\
+    display:flex;\
+    background-color:grey;\
+}\
+\
+</style>\
+\
+<nav class="navbar">\
+    <a href="./index.html">HOME</a> \
+    <a href="./Projects.html">PROJEKTE</a> \
+    <a href="./About.html">ABOUT</a> \
+</nav>\
+');
+
