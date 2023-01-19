8047d19048e6f9ead729d5464d1c11f421abcf7f
Es-Tessinerbrot <122873863+Es-Tessinerbrot@users.noreply.github.com>
2023-01-19 17:47:30 +0100

    padding navbar

--- Webpage/inc/navbar/navbar.js
+++ Webpage/inc/navbar/navbar.js
@@ -8,12 +8,15 @@ document.write('\
     text-decoration: none;\
     font-size: 15px;\
     font-family: monospace;\
+    height:30px;\
+    margin-top:5px;\
 }\
 .navbar > a:hover{\
     background-color: #333;\
 }\
 .navbar{\
     text-align:center;\
+    height:30px;\
     display:flex;\
     background-color:#222;\
 }\
@@ -24,6 +27,6 @@ document.write('\
     <a href="./index.html">HOME</a> \
     <a href="./Projects.html">PROJEKTE</a> \
     <a href="./About.html">ABOUT</a> \
-    <a href="./social.html">Social</a> \
+    <a href="./social.html">SOCIAL</a> \
 </nav>\
 ');
