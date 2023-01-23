c60e5e60c92959d1436aa1ee60f4e03a59b9bb73
ftave <fabiomeo.tavernini@gmail.com>
2023-01-23 11:58:47 +0100

    updated paths to directories

---
+++ Webpage/projects/inc/navbar/navbar.js
@@ -0,0 +1,31 @@
+document.write('\
+\
+<style>\
+\
+.navbar > a {\
+    width: 100%;\
+    color: white;\
+    text-decoration: none;\
+    font-size: 15px;\
+    font-family: monospace;\
+    padding-top: 6px;\
+}\
+.navbar > a:hover{\
+    background-color: #333;\
+}\
+.navbar{\
+    text-align:center;\
+    height:30px;\
+    display:flex;\
+    background-color:#222;\
+}\
+\
+</style>\
+\
+<nav class="navbar">\
+    <a id="navindex" href="../../index.html">HOME</a> \
+    <a id="navprojects" href="../../projects.html">PROJEKTE</a> \
+    <a id="navabout" href="../../about.html">ABOUT</a> \
+    <a id="navsocial" href="../../social.html">SOCIAL</a> \
+</nav>\
+');
