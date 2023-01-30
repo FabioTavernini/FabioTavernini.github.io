52d04b417b308135f8be54660054437f10dc73f1
Es-Tessinerbrot <122873863+Es-Tessinerbrot@users.noreply.github.com>
2023-01-30 11:01:43 +0100

    no xhr query

--- Webpage/projects/photography/photography.js
+++ Webpage/projects/photography/photography.js
@@ -1,19 +1,13 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "../../img/projects/photography", true);
-xhr.responseType = 'document';
-xhr.onload = () => {
-  if (xhr.status === 200) {
-    var elements = xhr.response.getElementsByTagName("a");
-    for (x of elements) {
-      if ( x.href.match(/\.(JPG|jpe?g|png|gif)$/) ) { 
-          let img = document.createElement("img");
-          img.src = x.href;
-          document.getElementById("images").appendChild(img);
-      } 
-    };
-  } 
-  else {
-    alert('Request failed. Returned status of ' + xhr.status);
-  }
-}
-xhr.send()
+var folder = "../../img/projects/photography/";
+var fileextension = ".JPG";
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(JPG|jpe?g|png|gif)$/) ) { 
+                $("#images").append( "<img src='"+ folder + val +"'>" );
+            } 
+        });
+    }
+});
