1360091f35acf3e4856ceae215d8ea6f587f12cb
Es-Tessinerbrot <122873863+Es-Tessinerbrot@users.noreply.github.com>
2023-01-30 10:19:44 +0100

    dynamically add img from folder to webpage with php

---
+++ Webpage/projects/photography/photography.js
@@ -0,0 +1,13 @@
+var folder = "../../img/projects/photography/";
+
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                $("body").append( "<img src='"+ folder + val +"'>" );
+            } 
+        });
+    }
+});
