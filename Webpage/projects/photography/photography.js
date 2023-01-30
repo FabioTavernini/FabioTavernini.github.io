edccd08e83431b3a8ab56cc7d3fcc4fe89e91a87
Es-Tessinerbrot <122873863+Es-Tessinerbrot@users.noreply.github.com>
2023-01-30 11:07:30 +0100

    edited js

--- Webpage/projects/photography/photography.js
+++ Webpage/projects/photography/photography.js
@@ -1,13 +1,17 @@
 var folder = "../../img/projects/photography/";
-var fileextension = ".JPG";
 
+var dir = "../../img/projects/photography/";
+var fileextension = ".JPG";
 $.ajax({
-    url : folder,
-    success: function (data) {
-        $(data).find("a").attr("href", function (i, val) {
-            if( val.match(/\.(JPG|jpe?g|png|gif)$/) ) { 
-                $("#images").append( "<img src='"+ folder + val +"'>" );
-            } 
-        });
-    }
+  //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+  url: dir,
+  success: function (data) {
+    //Lsit all png file names in the page
+    $(data).find("a:contains(" + fileextension + ")").each(function () {
+      var filename = this.href.replace(window.location.pathname, "").replace("http://", "");
+      var pathName = filename.substring(filename.lastIndexOf('/') + 1);
+      $("#images").append($("<img src=" + dir + pathName + "></img>"));
+      console.log(dir + pathName);
+    });
+  }
 });
