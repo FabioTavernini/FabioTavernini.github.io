9b40d7823b294a48ea5bee9ace4c9e83d765fd7d
ftave <fabiomeo.tavernini@gmail.com>
2023-02-10 14:06:34 +0100

    added lax elements to test branch

--- JS/scripthome.js
+++ JS/scripthome.js
@@ -1,37 +1,28 @@
-// function reveal() {
-//   var reveals = document.querySelectorAll(".reveal");
-
-//   for (var i = 0; i < reveals.length; i++) {
-//     var windowHeight = window.innerHeight;
-//     var elementTop = reveals[i].getBoundingClientRect().top;
-//     var elementVisible = 150;
-
-//     if (elementTop < windowHeight - elementVisible) {
-//       reveals[i].classList.add("active");
-//     } else {
-//       reveals[i].classList.remove("active");
-//     }
-//   }
-// }
-
-// window.addEventListener("scroll", reveal);
-
-
 window.onload = function () {
   lax.init()
 
   // Add a driver that we use to control our animations
   lax.addDriver('scrollY', function () {
-    return window.scrollY
+      return window.scrollY
   })
 
   // Add animation bindings to elements
-  // lax.addElements('.selector', {
-  //   scrollY: {
-  //     translateX: [
-  //       ["elInY", "elCenterY", "elOutY"],
-  //       [0, 'screenWidth/2', 'screenWidth'],
-  //     ]
-  //   }
-  // })
+  lax.addElements('.selector', {
+      scrollY: {
+          translateX: [
+              ["elInY", "elCenterY", "elOutY"],
+              [0, 'pageWidth/2', 'pageWidth'],
+          ]
+      }
+  })
+
+//   lax.addElements('.selector2', {
+//     scrollY: {
+//         translateX: [
+//             [ "elOutY", "elCenterY", "elInY"],
+//             ['screenWidth','screenWidth/2',0],
+//         ]
+//     }
+// })
+
 }
