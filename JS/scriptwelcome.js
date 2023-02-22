3fb8cd65879c9f01d87d9636e03cfe7c8bef668e
ftave <fabiomeo.tavernini@gmail.com>
2023-02-22 15:52:30 +0100

    fixed safari input issue

--- JS/scriptwelcome.js
+++ JS/scriptwelcome.js
@@ -1,17 +1,17 @@
 // Add event listener on keydown
-window.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('keydown', (event) => {
     var name = event.key;
     var code = event.code;
 
     if (name != '') {
-
         location.href = "./webpage/index.html";
-
     }
-
 });
 
-window.addEventListener("click", function(event) {
-    location.href = "./webpage/index.html";
-});
-
+if (document.readyState === 'complete') {
+    callback();
+  } else {
+    window.addEventListener("click", function(event) {
+        location.href = "./webpage/index.html";
+    });
+  }
