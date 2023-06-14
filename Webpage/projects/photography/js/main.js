1b69f18f0c91f0f03fc0f8d1a5f858eca5b8b94c
Tavernini Fabio <Fabio.Tavernini@ksd.ch>
2023-06-14 15:14:22 +0200

    feat: added svg to preload

--- Webpage/projects/photography/js/main.js
+++ Webpage/projects/photography/js/main.js
@@ -9,24 +9,24 @@
  * http://www.codrops.com
  */
 
-;(function(window) {
+; (function (window) {
 
 	'use strict';
 
 	var support = { transitions: Modernizr.csstransitions },
 		// transition end event name
 		transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' },
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		onEndTransition = function( el, callback ) {
-			var onEndCallbackFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.target != this ) return;
-					this.removeEventListener( transEndEventName, onEndCallbackFn );
+		transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+		onEndTransition = function (el, callback) {
+			var onEndCallbackFn = function (ev) {
+				if (support.transitions) {
+					if (ev.target != this) return;
+					this.removeEventListener(transEndEventName, onEndCallbackFn);
 				}
-				if( callback && typeof callback === 'function' ) { callback.call(this); }
+				if (callback && typeof callback === 'function') { callback.call(this); }
 			};
-			if( support.transitions ) {
-				el.addEventListener( transEndEventName, onEndCallbackFn );
+			if (support.transitions) {
+				el.addEventListener(transEndEventName, onEndCallbackFn);
 			}
 			else {
 				onEndCallbackFn();
@@ -36,14 +36,14 @@
 	/**
 	 * some helper functions
 	 */
-	
+
 	function throttle(fn, delay) {
 		var allowSample = true;
 
-		return function(e) {
+		return function (e) {
 			if (allowSample) {
 				allowSample = false;
-				setTimeout(function() { allowSample = true; }, delay);
+				setTimeout(function () { allowSample = true; }, delay);
 				fn(e);
 			}
 		};
@@ -51,15 +51,15 @@
 
 	function nextSibling(el) {
 		var nextSibling = el.nextSibling;
-		while(nextSibling && nextSibling.nodeType != 1) {
+		while (nextSibling && nextSibling.nodeType != 1) {
 			nextSibling = nextSibling.nextSibling
 		}
 		return nextSibling;
 	}
 
-	function extend( a, b ) {
-		for( var key in b ) { 
-			if( b.hasOwnProperty( key ) ) {
+	function extend(a, b) {
+		for (var key in b) {
+			if (b.hasOwnProperty(key)) {
 				a[key] = b[key];
 			}
 		}
@@ -71,9 +71,9 @@
 	 */
 	function GridFx(el, options) {
 		this.gridEl = el;
-		this.options = extend( {}, this.options );
-		extend( this.options, options );
-		
+		this.options = extend({}, this.options);
+		extend(this.options, options);
+
 		this.items = [].slice.call(this.gridEl.querySelectorAll('.grid__item'));
 		this.previewEl = nextSibling(this.gridEl);
 		this.isExpanded = false;
@@ -88,28 +88,28 @@
 	 * options
 	 */
 	GridFx.prototype.options = {
-		pagemargin : 0,
+		pagemargin: 0,
 		// x and y can have values from 0 to 1 (percentage). If negative then it means the alignment is left and/or top rather than right and/or bottom
 		// so, as an example, if we want our large image to be positioned vertically on 25% of the screen and centered horizontally the values would be x:1,y:-0.25
-		imgPosition : { x : 1, y : 1 },
-		onInit : function(instance) { return false; },
-		onResize : function(instance) { return false; },
-		onOpenItem : function(instance, item) { return false; },
-		onCloseItem : function(instance, item) { return false; },
-		onExpand : function() { return false; }
+		imgPosition: { x: 1, y: 1 },
+		onInit: function (instance) { return false; },
+		onResize: function (instance) { return false; },
+		onOpenItem: function (instance, item) { return false; },
+		onCloseItem: function (instance, item) { return false; },
+		onExpand: function () { return false; }
 	}
 
-	GridFx.prototype._init = function() {
+	GridFx.prototype._init = function () {
 		// callback
 		this.options.onInit(this);
 
 		var self = this;
 		// init masonry after all images are loaded
-		imagesLoaded( this.gridEl, function() {
+		imagesLoaded(this.gridEl, function () {
 			// initialize masonry
 			new Masonry(self.gridEl, {
 				itemSelector: '.grid__item',
-				isFitWidth : true
+				isFitWidth: true
 			});
 			// show grid after all images (thumbs) are loaded
 			classie.add(self.gridEl, 'grid--loaded');
@@ -127,24 +127,24 @@
 	 */
 	GridFx.prototype._initEvents = function () {
 		var self = this,
-			clickEvent = (document.ontouchstart!==null ? 'click' : 'touchstart');
+			clickEvent = (document.ontouchstart !== null ? 'click' : 'touchstart');
 
-		this.items.forEach(function(item) {
-			var touchend = function(ev) {
-					ev.preventDefault();
-					self._openItem(ev, item);
-					item.removeEventListener('touchend', touchend);	
+		this.items.forEach(function (item) {
+			var touchend = function (ev) {
+				ev.preventDefault();
+				self._openItem(ev, item);
+				item.removeEventListener('touchend', touchend);
+			},
+				touchmove = function (ev) {
+					item.removeEventListener('touchend', touchend);
 				},
-				touchmove = function(ev) {
-					item.removeEventListener('touchend', touchend);	
-				},
-				manageTouch = function() {
+				manageTouch = function () {
 					item.addEventListener('touchend', touchend);
 					item.addEventListener('touchmove', touchmove);
 				};
 
-			item.addEventListener(clickEvent, function(ev) {
-				if(clickEvent === 'click') {
+			item.addEventListener(clickEvent, function (ev) {
+				if (clickEvent === 'click') {
 					ev.preventDefault();
 					self._openItem(ev, item);
 				}
@@ -155,11 +155,30 @@
 		});
 
 		// close expanded image
-		this.closeCtrl.addEventListener('click', function() {
-			self._closeItem(); 
+		this.closeCtrl.addEventListener('click', function () {
+			self._closeItem();
 		});
 
-		window.addEventListener('resize', throttle(function(ev) {
+
+
+		document.addEventListener('keydown', function (e) {
+			self._closeItem();
+		});
+
+
+		document.addEventListener('click', function (e) {
+			self._closeItem();
+		});
+
+
+
+
+
+
+
+
+
+		window.addEventListener('resize', throttle(function (ev) {
 			// callback
 			self.options.onResize(self);
 		}, 10));
@@ -168,8 +187,8 @@
 	/**
 	 * open a grid item
 	 */
-	GridFx.prototype._openItem = function(ev, item) {
-		if( this.isAnimating || this.isExpanded ) return;
+	GridFx.prototype._openItem = function (ev, item) {
+		if (this.isAnimating || this.isExpanded) return;
 		this.isAnimating = true;
 		this.isExpanded = true;
 
@@ -182,16 +201,16 @@
 
 		// set the src of the original image element (large image)
 		this._setOriginal(item.querySelector('a').getAttribute('href'));
-		
+
 		// callback
 		this.options.onOpenItem(this, item);
 
 		// set the clone image
 		this._setClone(gridImg.src, {
-			width : gridImg.offsetWidth,
-			height : gridImg.offsetHeight,
-			left : gridImgOffset.left,
-			top : gridImgOffset.top
+			width: gridImg.offsetWidth,
+			height: gridImg.offsetHeight,
+			left: gridImgOffset.left,
+			top: gridImgOffset.top
 		});
 
 		// hide original grid item
@@ -200,10 +219,10 @@
 		// calculate the transform value for the clone to animate to the full image view
 		var win = this._getWinSize(),
 			originalSizeArr = item.getAttribute('data-size').split('x'),
-			originalSize = {width: originalSizeArr[0], height: originalSizeArr[1]},
-			dx = ((this.options.imgPosition.x > 0 ? 1-Math.abs(this.options.imgPosition.x) : Math.abs(this.options.imgPosition.x)) * win.width + this.options.imgPosition.x * win.width/2) - gridImgOffset.left - 0.5 * gridImg.offsetWidth,
-			dy = ((this.options.imgPosition.y > 0 ? 1-Math.abs(this.options.imgPosition.y) : Math.abs(this.options.imgPosition.y)) * win.height + this.options.imgPosition.y * win.height/2) - gridImgOffset.top - 0.5 * gridImg.offsetHeight,
-			z = Math.min( Math.min(win.width*Math.abs(this.options.imgPosition.x) - this.options.pagemargin, originalSize.width - this.options.pagemargin)/gridImg.offsetWidth, Math.min(win.height*Math.abs(this.options.imgPosition.y) - this.options.pagemargin, originalSize.height - this.options.pagemargin)/gridImg.offsetHeight );
+			originalSize = { width: originalSizeArr[0], height: originalSizeArr[1] },
+			dx = ((this.options.imgPosition.x > 0 ? 1 - Math.abs(this.options.imgPosition.x) : Math.abs(this.options.imgPosition.x)) * win.width + this.options.imgPosition.x * win.width / 2) - gridImgOffset.left - 0.5 * gridImg.offsetWidth,
+			dy = ((this.options.imgPosition.y > 0 ? 1 - Math.abs(this.options.imgPosition.y) : Math.abs(this.options.imgPosition.y)) * win.height + this.options.imgPosition.y * win.height / 2) - gridImgOffset.top - 0.5 * gridImg.offsetHeight,
+			z = Math.min(Math.min(win.width * Math.abs(this.options.imgPosition.x) - this.options.pagemargin, originalSize.width - this.options.pagemargin) / gridImg.offsetWidth, Math.min(win.height * Math.abs(this.options.imgPosition.y) - this.options.pagemargin, originalSize.height - this.options.pagemargin) / gridImg.offsetHeight);
 
 		// apply transform to the clone
 		this.cloneImg.style.WebkitTransform = 'translate3d(' + dx + 'px, ' + dy + 'px, 0) scale3d(' + z + ', ' + z + ', 1)';
@@ -211,12 +230,12 @@
 
 		// add the description if any
 		var descriptionEl = item.querySelector('.description');
-		if( descriptionEl ) {
+		if (descriptionEl) {
 			this.previewDescriptionEl.innerHTML = descriptionEl.innerHTML;
 		}
 
 		var self = this;
-		setTimeout(function() { 
+		setTimeout(function () {
 			// controls the elements inside the expanded view
 			classie.add(self.previewEl, 'preview--open');
 			// callback
@@ -224,15 +243,15 @@
 		}, 0);
 
 		// after the clone animates..
-		onEndTransition(this.cloneImg, function() {
+		onEndTransition(this.cloneImg, function () {
 			// when the original/large image is loaded..
-			imagesLoaded(self.originalImg, function() {
+			imagesLoaded(self.originalImg, function () {
 				// close button just gets shown after the large image gets loaded
 				classie.add(self.previewEl, 'preview--image-loaded');
 				// animate the opacity to 1
 				self.originalImg.style.opacity = 1;
 				// and once that's done..
-				onEndTransition(self.originalImg, function() {
+				onEndTransition(self.originalImg, function () {
 					// reset cloneImg
 					self.cloneImg.style.opacity = 0;
 					self.cloneImg.style.WebkitTransform = 'translate3d(0,0,0) scale3d(1,1,1)';
@@ -240,21 +259,21 @@
 
 					self.isAnimating = false;
 				});
-				
-			});	
+
+			});
 		});
 	};
 
 	/**
 	 * create/set the original/large image element
 	 */
-	GridFx.prototype._setOriginal = function(src) {
-		if( !src ) {
+	GridFx.prototype._setOriginal = function (src) {
+		if (!src) {
 			this.originalImg = document.createElement('img');
 			this.originalImg.className = 'original';
 			this.originalImg.style.opacity = 0;
-			this.originalImg.style.maxWidth = 'calc(' + parseInt(Math.abs(this.options.imgPosition.x)*100) + 'vw - ' + this.options.pagemargin + 'px)';
-			this.originalImg.style.maxHeight = 'calc(' + parseInt(Math.abs(this.options.imgPosition.y)*100) + 'vh - ' + this.options.pagemargin + 'px)';
+			this.originalImg.style.maxWidth = 'calc(' + parseInt(Math.abs(this.options.imgPosition.x) * 100) + 'vw - ' + this.options.pagemargin + 'px)';
+			this.originalImg.style.maxHeight = 'calc(' + parseInt(Math.abs(this.options.imgPosition.y) * 100) + 'vh - ' + this.options.pagemargin + 'px)';
 			// need it because of firefox
 			this.originalImg.style.WebkitTransform = 'translate3d(0,0,0) scale3d(1,1,1)';
 			this.originalImg.style.transform = 'translate3d(0,0,0) scale3d(1,1,1)';
@@ -268,8 +287,8 @@
 	/**
 	 * create/set the clone image element
 	 */
-	GridFx.prototype._setClone = function(src, settings) {
-		if( !src ) {
+	GridFx.prototype._setClone = function (src, settings) {
+		if (!src) {
 			this.cloneImg = document.createElement('img');
 			this.cloneImg.className = 'clone';
 			src = '';
@@ -279,10 +298,10 @@
 		else {
 			this.cloneImg.style.opacity = 1;
 			// set top/left/width/height of grid item's image to the clone
-			this.cloneImg.style.width = settings.width  + 'px';
-			this.cloneImg.style.height = settings.height  + 'px';
-			this.cloneImg.style.top = settings.top  + 'px';
-			this.cloneImg.style.left = settings.left  + 'px';
+			this.cloneImg.style.width = settings.width + 'px';
+			this.cloneImg.style.height = settings.height + 'px';
+			this.cloneImg.style.top = settings.top + 'px';
+			this.cloneImg.style.left = settings.left + 'px';
 		}
 
 		this.cloneImg.setAttribute('src', src);
@@ -291,8 +310,8 @@
 	/**
 	 * closes the original/large image view
 	 */
-	GridFx.prototype._closeItem = function() {
-		if( !this.isExpanded || this.isAnimating ) return;
+	GridFx.prototype._closeItem = function () {
+		if (!this.isExpanded || this.isAnimating) return;
 		this.isExpanded = false;
 		this.isAnimating = true;
 
@@ -304,7 +323,7 @@
 
 		classie.remove(this.previewEl, 'preview--open');
 		classie.remove(this.previewEl, 'preview--image-loaded');
-		
+
 		// callback
 		this.options.onCloseItem(this, gridItem);
 
@@ -313,15 +332,15 @@
 
 		// set the transform to the original/large image
 		var win = this._getWinSize(),
-			dx = gridImgOffset.left + gridImg.offsetWidth/2 - ((this.options.imgPosition.x > 0 ? 1-Math.abs(this.options.imgPosition.x) : Math.abs(this.options.imgPosition.x)) * win.width + this.options.imgPosition.x * win.width/2),
-			dy = gridImgOffset.top + gridImg.offsetHeight/2 - ((this.options.imgPosition.y > 0 ? 1-Math.abs(this.options.imgPosition.y) : Math.abs(this.options.imgPosition.y)) * win.height + this.options.imgPosition.y * win.height/2),
-			z = gridImg.offsetWidth/this.originalImg.offsetWidth;
+			dx = gridImgOffset.left + gridImg.offsetWidth / 2 - ((this.options.imgPosition.x > 0 ? 1 - Math.abs(this.options.imgPosition.x) : Math.abs(this.options.imgPosition.x)) * win.width + this.options.imgPosition.x * win.width / 2),
+			dy = gridImgOffset.top + gridImg.offsetHeight / 2 - ((this.options.imgPosition.y > 0 ? 1 - Math.abs(this.options.imgPosition.y) : Math.abs(this.options.imgPosition.y)) * win.height + this.options.imgPosition.y * win.height / 2),
+			z = gridImg.offsetWidth / this.originalImg.offsetWidth;
 
 		this.originalImg.style.WebkitTransform = 'translate3d(' + dx + 'px, ' + dy + 'px, 0) scale3d(' + z + ', ' + z + ', 1)';
-		this.originalImg.style.transform = 'translate3d(' + dx + 'px, ' + dy + 'px, 0) scale3d(' + z + ', ' + z + ', 1)';	
-		
+		this.originalImg.style.transform = 'translate3d(' + dx + 'px, ' + dy + 'px, 0) scale3d(' + z + ', ' + z + ', 1)';
+
 		// once that's done..
-		onEndTransition(this.originalImg, function() {
+		onEndTransition(this.originalImg, function () {
 			// clear description
 			self.previewDescriptionEl.innerHTML = '';
 
@@ -329,10 +348,10 @@
 			classie.remove(gridItem, 'grid__item--current');
 
 			// fade out the original image
-			setTimeout(function() { self.originalImg.style.opacity = 0;	}, 60);
+			setTimeout(function () { self.originalImg.style.opacity = 0; }, 60);
 
 			// and after that
-			onEndTransition(self.originalImg, function() {
+			onEndTransition(self.originalImg, function () {
 				// reset original/large image
 				classie.remove(self.originalImg, 'animate');
 				self.originalImg.style.WebkitTransform = 'translate3d(0,0,0) scale3d(1,1,1)';
@@ -346,7 +365,7 @@
 	/**
 	 * gets the window sizes
 	 */
-	GridFx.prototype._getWinSize = function() {
+	GridFx.prototype._getWinSize = function () {
 		return {
 			width: document.documentElement.clientWidth,
 			height: window.innerHeight
