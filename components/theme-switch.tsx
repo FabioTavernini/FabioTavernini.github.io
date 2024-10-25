f8181a25b6be14657ff0f84548931cefaf339b6f
Fabio Tavernini <fabio.tavernini@itsh.ch>
2024-10-25 08:40:01 +0200

    blobal install of next module so next install can be used

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -7,8 +7,6 @@ import { useTheme } from "next-themes";
 import { useIsSSR } from "@react-aria/ssr";
 import clsx from "clsx";
 
-import { SunFilledIcon, MoonFilledIcon } from "@/components/icons";
-
 export interface ThemeSwitchProps {
   className?: string;
   classNames?: SwitchProps["classNames"];
@@ -70,11 +68,6 @@ export const ThemeSwitch: FC<ThemeSwitchProps> = ({
           ),
         })}
       >
-        {!isSelected || isSSR ? (
-          <SunFilledIcon size={22} />
-        ) : (
-          <MoonFilledIcon size={22} />
-        )}
       </div>
     </Component>
   );
