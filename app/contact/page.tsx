a29302507b20c25f49534a4e26c8dc7887983c86
Fabio Tavernini <fabio.tavernini@itsh.ch>
2024-11-01 14:34:23 +0100

    testing netlify form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,14 +4,25 @@ import React from "react";
 
 const FeedbackForm = () => {
   const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget); // Use event.currentTarget to get the form
-    await fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData as any).toString(), // Use 'as any' if TypeScript complains
-    });
-    // Success and error handling ...
+    event.preventDefault(); // Prevent default form submission
+    const formData = new FormData(event.currentTarget); // Get form data
+
+    try {
+      const response = await fetch("/__forms.html", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData as any).toString(),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      console.log("Form submitted successfully!"); // Log success
+      // Optionally, show a success message or redirect the user
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error); // Log any errors
+    }
   };
 
   return (
