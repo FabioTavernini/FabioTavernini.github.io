75d5740d2888cea2bcf3609c9d125b7360749997
Fabio Tavernini <fabio.tavernini@itsh.ch>
2024-11-01 12:53:38 +0100

    testing netlify forms

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,25 +10,26 @@ export default function Contact() {
       <h2 className="m-4 text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">Contact</h2>
 
 
-      <form name="contactform" method="POST" data-netlify="true">
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+      {/* Hidden form for Netlify */}
+      <form name="contact" method="POST" data-netlify="true" className="hidden">
+        <input type="hidden" name="contactform" value="contact" />
+        <input type="hidden" name="email" />
+        <input type="hidden" name="subject" />
+        <input type="hidden" name="message" />
+      </form>
+
+
+
+      <form name="contactform" method="POST" className="pr-5">
+
+        <input type="hidden" name="form-name" value="contactform" />
+
+        <Input name="email" type="email" label="Email" placeholder="Enter your email" className="m-4" />
+        <Input name="subject" type="text" label="Subject" className="m-4" />
+        <Textarea name="message" label="Your message for me" minRows={15} className="m-4" />
+
+        <Button size="md" color="primary" type="submit" className="m-4">Submit</Button>
+
       </form>
 
     </div>
