f9004a605ac8a5edba168e2586b29dc573b93997
Fabio Tavernini <fabio.tavernini@itsh.ch>
2024-11-01 13:48:33 +0100

    changed name of form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,31 +1,26 @@
-"use client"
-
 import { Button } from "@nextui-org/button";
+import { Input, Textarea } from "@nextui-org/input";
 
-export default function ContactForm() {
+export default function Contact() {
+
   return (
-    
-    <form name="contact" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
 
-      <p>
-        <label htmlFor="name">Your Name:</label>
-        <input type="text" name="name" id="name" required />
-      </p>
+    <div className="">
 
-      <p>
-        <label htmlFor="email">Your Email:</label>
-        <input type="email" name="email" id="email" required />
-      </p>
+      <h2 className="m-4 text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">Contact</h2>
 
-      <p>
-        <label htmlFor="message">Message:</label>
-        <textarea name="message" id="message" required></textarea>
-      </p>
 
-      <p>
-        <Button type="submit" className="submit-button">Send</Button>
-      </p>
-    </form>
+      <form name="contact" className="pr-5">
+
+        <Input name="email" type="email" label="Email" placeholder="Enter your email" className="m-4" />
+        <Input name="subject" type="text" label="Subject" className="m-4" />
+        <Textarea name="message" label="Your message for me" minRows={15} className="m-4" />
+
+        <Button size="md" color="primary" type="submit" className="m-4">Submit</Button>
+
+      </form>
+
+    </div>
+
   );
 }
