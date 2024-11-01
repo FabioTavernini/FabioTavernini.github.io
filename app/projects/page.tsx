7b11bd3362311daa272597e7557d6d163b88786d
Fabio Tavernini <fabio.tavernini@itsh.ch>
2024-11-01 12:24:21 +0100

    changed to masonry grid

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,139 +1,108 @@
 "use client"
 
-import {Card, CardHeader, CardBody, CardFooter, Image, Button} from "@nextui-org/react";  
+import { SiCreality } from "@icons-pack/react-simple-icons";
+import { Card, CardHeader, CardBody, CardFooter, Image, Button } from "@nextui-org/react";
 import Link from "next/link";
 import { FaGithub } from 'react-icons/fa';
 
 export default function ProjectsPage() {
-
 
   const handleButtonClick = () => {
     // Redirect to the /projects page
     window.open("https://github.com/FabioTavernini/FabioTavernini.github.io", "_blank");
   };
 
-
-
-
   return (
 
     <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
 
+      <Link className="col-span-12 sm:col-span-4 h-[300px]" href={"/projects/photography"}>
 
-<Link className="col-span-12 sm:col-span-4 h-[300px]" href={"/projects/photography"}>
+        <Card className="col-span-12 sm:col-span-4 h-[300px]">
 
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-   
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Photography</p>
-        <h4 className="text-white font-medium text-large">Gallery</h4>
-      </CardHeader>
-      
-      
-      <Image
-      
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src="/images/antelope_canyon.jpg"
-      />  
-      
-       </Card>
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <p className="text-tiny text-black uppercase font-normal">Photography</p>
+            <h4 className="text-white font-medium text-large">Gallery</h4>
+          </CardHeader>
 
-</Link>
+          <Image
 
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="/images/thumbnails/antelope_canyon.jpg"
+            
+          />
 
+        </Card>
+      </Link>
 
+      <Card className="col-span-12 sm:col-span-4 h-[300px]">
+        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">some of my work</p>
+          <h4 className="text-white font-medium text-large">Tinkering</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card background"
+          className="z-0 w-full h-full object-cover"
+          src="https://nextui.org/images/card-example-3.jpeg"
+        />
+      </Card>
 
+      <Card className="col-span-12 sm:col-span-4 h-[300px]">
+        <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
+          <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card background"
+          className="z-0 w-full h-full object-cover"
+          src="https://nextui.org/images/card-example-2.jpeg"
+        />
+      </Card>
 
+      <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
+        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+          <p className="text-tiny text-black/60 uppercase font-bold">STL Files & More</p>
+          <h4 className="text-black font-medium text-2xl">3D-Printing</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Card example background"
+          className="z-0 w-full h-full scale-125 -translate-y-6 object-cover blur-[3px]"
+          src="https://media.elv.com/pdsquare/253165_hb.jpg"
+        />
+        <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+          <div>
+            <p className="text-black text-tiny">Coming soon.</p>
+          </div>
+        </CardFooter>
+      </Card>
 
+      <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
+        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+          <p className="text-tiny text-white/60 uppercase font-bold">NextJS, TailwindCSS & Netlify</p>
+          <h4 className="text-white/90 font-medium text-xl">This website</h4>
+        </CardHeader>
+        <Image
+          removeWrapper
+          alt="Relaxing app background"
+          className="z-0 w-full h-full object-cover"
+          src="/gifs/githubcard.gif"
+        />
+        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+          <div className="flex flex-grow gap-2 items-center">
 
-
-
-
-
-      
-    
- 
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
-        <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-3.jpeg"
-      />
-    </Card>
-
-
-
-
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
-        <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-2.jpeg"
-      />
-    </Card>
-
-
-
-
-    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">STL Files & More</p>
-        <h4 className="text-black font-medium text-2xl">3D-Printing</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card example background"
-        className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-        src="https://nextui.org/images/card-example-6.jpeg"
-      />
-      <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-        <div>
-          <p className="text-black text-tiny">Available soon.</p>
-        </div>
-      </CardFooter>
-    </Card>
-
-
-
-    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">NextJS, Tailwind & Netlify</p>
-        <h4 className="text-white/90 font-medium text-xl">This website</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Relaxing app background"
-        className="z-0 w-full h-full object-cover"
-        src="/gifs/githubcard.gif"
-      />
-      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-        <div className="flex flex-grow gap-2 items-center">
-
- 
-        </div>
-        <Button onClick={handleButtonClick} endContent={<FaGithub size={20} />} radius="full" size="sm">
+          </div>
+          <Button onClick={handleButtonClick} endContent={<FaGithub size={20} />} radius="full" size="sm">
             View on GitHub
           </Button>
-                </CardFooter>
-                
-    </Card>
+        </CardFooter>
 
+      </Card>
 
-
-
-
-  </div>
+    </div>
   );
 }
--- app/projects/photography/page.tsx
+++ app/projects/photography/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { Button } from "@nextui-org/button";
+import Masonry from 'react-masonry-css';
 
 const images = [
   "parking-ticket.jpg",
@@ -22,7 +23,7 @@ const images = [
   "roadstop.jpg",
   "stahlgiesserei.jpg",
   "plitvice2.jpg",
-].map((img) => `/images/thumbnails/${img}`); // Update paths to thumbnails
+].map((img) => `/images/thumbnails/${img}`);
 
 const ImageGallery = () => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
@@ -30,7 +31,7 @@ const ImageGallery = () => {
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "Escape") {
-        setSelectedImage(null); // Clear selected image on Escape key press
+        setSelectedImage(null);
       }
     };
 
@@ -41,40 +42,40 @@ const ImageGallery = () => {
   }, []);
 
   return (
-
     <div className="flex flex-col items-center">
-      
       <h2 className="text-xl font-bold leading-7 text-white sm:truncate sm:text-2xl sm:tracking-tight">Image Gallery</h2>
+      <h3 className="bg-gradient-to-r from-yellow-400 to-red-600 text-transparent bg-clip-text font-bold text-xs leading-2 sm:text-xl sm:tracking-tight">
+        Be patient when loading an image. When clicking on one, the "uncompressed" file is loaded :)
+      </h3>
 
-      <h3 className="bg-gradient-to-r from-yellow-400 to-red-600 text-transparent bg-clip-text font-bold text-xs leading-2 sm:text-xl sm:tracking-tight">Be patient when loading an image. When clicking on one, the "uncompressed" file is loaded :)</h3>
-  
-
-      {/* Image Grid */}
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-[98vw] justify-center">
+      {/* Masonry Grid */}
+      <Masonry
+        breakpointCols={{ default: 4, 1100: 3, 700: 2, 500: 1 }}
+        className="flex justify-center w-[97vw]"
+        columnClassName="masonry-grid-column"
+      >
         {images.map((src, index) => (
           <Button
             key={index}
-            onPress={() => setSelectedImage(src.replace('/thumbnails/', '/'))} 
-            className="h-96 w-64 lg:h-auto lg:w-auto object-cover cursor-pointer rounded-lg shadow-lg hover:shadow-xl justify-self-center transition-shadow duration-300"
-            style={{ background: 'none', border: 'none' }}
+            onPress={() => setSelectedImage(src.replace('/thumbnails/', '/'))}
+            className="rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 w-full h-auto object-cover rounded-lg"
+            style={{ background: 'none', border: 'none', marginBottom: '10px' }} // Add margin for spacing
           >
             <img
               src={src}
-              className="h-96 w-64 lg:h-auto lg:w-auto object-cover cursor-pointer rounded-lg shadow-lg hover:shadow-xl justify-self-center transition-shadow duration-300"
-             
+              className="w-full h-auto object-cover rounded-lg"
             />
           </Button>
         ))}
-      </div>
-  
+      </Masonry>
+
       {/* Modal */}
       {selectedImage && (
         <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50">
           <div className="relative">
             <img
               src={selectedImage}
-              className="max-w-full max-h-screen rounded-lg justify-self-center"
-              
+              className="max-w-full max-h-screen rounded-lg"
             />
             <Button
               onPress={() => setSelectedImage(null)}
