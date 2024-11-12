b40e8e8ad49e0183d55e0f2d35e0939d51e3e91b
Fabio Tavernini <fabiomeo.tavernini@gmail.com>
2024-11-12 17:45:27 +0100

    added .webp image for performance

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,8 +20,7 @@ export default function ProjectsPage() {
 
         <Card className="col-span-12 sm:col-span-4 h-[300px]">
 
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-black uppercase font-normal">Photography</p>
+          <CardHeader className="absolute flex-col !items-start bg-black/50 justify-between">
             <h4 className="text-white font-medium text-large">Gallery</h4>
           </CardHeader>
 
@@ -30,7 +29,7 @@ export default function ProjectsPage() {
             removeWrapper
             alt="Card background"
             className="z-0 w-full h-full object-cover"
-            src="/images/thumbnails/antelope_canyon.jpg"
+            src="/images/thumbnails/antelope_canyon.webp"
             
           />
 
