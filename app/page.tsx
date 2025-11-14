fbfa6352c66e0be44d223b828c8c74878f398f95
FabioTavernini <fabiomeo.tavernini@gmail.com>
2025-11-14 15:26:38 +0100

    fix firefox issue (gpu rendering of blur svg) - @helblinglilly :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Code } from "@heroui/react";
-import Image from 'next/image';
-import me from '../public/images/BW.webp'
+import { Image } from "@heroui/image";
 import Link from "next/link";
 
 export default function HomePage() {
@@ -19,55 +18,10 @@ export default function HomePage() {
 
       <div className="flex flex-col items-center md:flex-row ml-5 w-[98vw]">
 
-        <svg width="0" height="0">
-          <filter
-            id="ambilight"
-            width="300%"
-            height="300%"
-            x="-0.75"
-            y="-0.75"
-            colorInterpolationFilters="sRGB"
-          >
-            <feOffset in="SourceGraphic" result="source-copy" />
-            <feColorMatrix
-              in="source-copy"
-              type="saturate"
-              values="3"
-              result="saturated-copy"
-            />
-            <feColorMatrix
-              in="saturated-copy"
-              type="matrix"
-              values="1 0 0 0 0
-                    0 1 0 0 0
-                    0 0 1 0 0
-                    33 33 33 101 -132"
-              result="bright-colors"
-            />
-            <feMorphology
-              in="bright-colors"
-              operator="dilate"
-              radius="10"
-              result="spread"
-            />
-            <feGaussianBlur
-              in="spread"
-              stdDeviation="30"
-              result="ambilight-light"
-            />
-            <feOffset in="SourceGraphic" result="source" />
-            <feComposite in="source" in2="ambilight-light" operator="over" />
-          </filter>
-        </svg>
-
         <Image
-          id="BW"
-          src={me}
-          alt="test"
-          className="rounded-lg md:w-1/2"
-          priority // LCP 
-          sizes="(max-width: 768px) 50vw, 33vw" // Ensure proper responsive sizes
-          placeholder="blur"
+          alt="Portrait of me :)"
+          src="/images/BW.webp"
+          className="rounded-lg w-full"
         />
 
         <div className="w-[85vw] m-5 md:w-full">
@@ -82,7 +36,9 @@ export default function HomePage() {
 
           <h2 className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-red-600">What i do</h2>
           <h3 className="text-gray-400">About me</h3>
-          <p>Im an Application Manager at <Link className="text-blue-500" href={"https://itsh.ch"}>ITSH.ch</Link> and am responsible for a number of legacy PHP apps, aswell as many diffrent Powershell automation Jobs. If you&apos;d like to see my CV, you can do so here: <Link className="text-blue-500" href={"https://tavernini.online/cv"}>CV</Link>.</p>
+          <p>
+            I'm an Application Manager at <Link className="text-blue-500" href="https://itsh.ch">ITSH.ch</Link>, responsible for a number of legacy PHP apps, various PowerShell automation jobs, and partially managing our internal VMware Tanzu Kubernetes platform (all on-prem) along with our GitOps workflows. If you&apos;d like to see my CV, you can do so here: <Link className="text-blue-500" href="https://tavernini.online/cv">CV</Link>.
+          </p>
 
           <br></br>
           <hr></hr>
