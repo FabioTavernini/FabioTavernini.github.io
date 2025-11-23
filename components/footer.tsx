11b1bcaa6868ee288b2e13b833ea544e12bd8785
FabioTavernini <fabiomeo.tavernini@gmail.com>
2025-11-23 22:03:10 +0100

    animate icons ;P

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,33 @@
+"use client";
+
 import { FaEnvelope, FaGithub, FaInstagram } from "react-icons/fa";
 import { SiWhatsapp } from "@icons-pack/react-simple-icons";
+import { motion } from "framer-motion";
 
 export const Footer = () => {
+  const icons = [
+    {
+      label: "GitHub",
+      href: "https://github.com/FabioTavernini",
+      icon: <FaGithub size={25} />,
+    },
+    {
+      label: "Instagram",
+      href: "https://instagram.com/F_Tavernini",
+      icon: <FaInstagram size={25} />,
+    },
+    {
+      label: "Mail",
+      href: "mailto:fabiomeo.tavernini@gmail.com",
+      icon: <FaEnvelope size={25} />,
+    },
+    {
+      label: "Whatsapp",
+      href: "https://wa.me/41774028394?text=pläuderle",
+      icon: <SiWhatsapp size={24} />,
+    },
+  ];
+
   return (
     <footer className="bg-black-800 text-white py-8 mt-16">
       <div className="container mx-auto px-4">
@@ -14,47 +40,26 @@ export const Footer = () => {
           </div>
 
           <div className="flex space-x-6">
-            <a
-              aria-label="GitHub"
-              href="https://github.com/FabioTavernini"
-              rel="noopener"
-              target="_blank"
-            >
-              <FaGithub size={25} />
-            </a>
-
-            <a
-              aria-label="Instagram"
-              href="https://instagram.com/F_Tavernini"
-              rel="noopener"
-              target="_blank"
-            >
-              <FaInstagram size={25} />
-            </a>
-
-            <a
-              aria-label="Mail"
-              href="mailto:fabiomeo.tavernini@gmail.com"
-              rel="noopener"
-              target="_blank"
-            >
-              <FaEnvelope size={25} />
-            </a>
-            <a
-              aria-label="Whatsapp"
-              href="https://wa.me/41774028394?text=pläuderle"
-              rel="noopener"
-              target="_blank"
-            >
-              <SiWhatsapp size={24} />
-            </a>
+            {icons.map(({ label, href, icon }) => (
+              <motion.a
+                key={label}
+                whileHover={{ y: -8, scale: 1.2 }}
+                transition={{ type: "spring", stiffness: 500 }}
+                aria-label={label}
+                href={href}
+                rel="noopener"
+                target="_blank"
+              >
+                {icon}
+              </motion.a>
+            ))}
           </div>
         </div>
 
         <div className="text-center mt-8">
           <p className="text-sm text-white-500">
-            Made with Next.js + React , TailwindCSS & ❤️ <br />
-            Hosted on netlify
+            Made with Next.js + React, TailwindCSS & ❤️ <br />
+            Hosted on Netlify
           </p>
         </div>
       </div>
